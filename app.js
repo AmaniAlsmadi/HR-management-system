285c10b1abcd190696007a996a4b890fa818c0b9
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-12 19:07:43 +0300

    task 10

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ function Employee (employeeId, fullName, department, level , image ){
 
  Employee.prototype.employeesTable = function(){
   
-  document.write (`<p> Full name : ${this.fullName}, Salary : ${this.salary} JD , Net salary : ${this.netSalary} JD  \n </p>`)
-  }
+ document.write (`<p> Full name : ${this.fullName}, Salary : ${this.salary} JD , Net salary : ${this.netSalary} JD </p> `)
+    
+}
 
 
 
@@ -45,19 +46,88 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
- const ghaziSamer = new Employee ( 1000 ,'Ghazi Samer','Administration','Senior');
- const lanaAli = new Employee (1001,'Lana Ali','Finance','Senior');
- const tamaraAyoub = new Employee ( 1002,'Tamara Ayoub','Marketing','Senior');
- const safiWalid = new Employee (1003,'Safi Walid','Administration','Mid-Senior');
- const omarZaid = new Employee (1004,'Omar Zaid','Development','Senior');
- const ranaSaleh = new Employee (1005,'Rana Saleh','Development','Junior');
- const hadiAhmad = new Employee (1006,'Hadi Ahmad','Finance','Mid-Senior');
+ const ghaziSamer = new Employee ( 1000 ,'Ghazi Samer','Administration','Senior', "./assets/Ghazi1.png" );
+ const lanaAli = new Employee (1001,'Lana Ali','Finance','Senior','./assets/Lana.jpg' );
+ const tamaraAyoub = new Employee ( 1002,'Tamara Ayoub','Marketing','Senior','./assets/Tamara.jpg' );
+ const safiWalid = new Employee (1003,'Safi Walid','Administration','Mid-Senior', './assets/Safi.jpg');
+ const omarZaid = new Employee (1004,'Omar Zaid','Development','Senior', './assets/Omer.jpg');
+ const ranaSaleh = new Employee (1005,'Rana Saleh','Development','Junior','./assets/Rana.jpg' );
+ const hadiAhmad = new Employee (1006,'Hadi Ahmad','Finance','Mid-Senior','./assets/Hadi.jpg' );
 
 
  for(let i = 0 ; i < allEmployee.length ; i++ ){
   allEmployee[i].salary();
   allEmployee[i].netSalary();
   allEmployee[i].employeesTable();
+}
+
+ //task10
+
+Employee.prototype.employeeCard = function() {
+ 
+
+  if( this.level.toLowerCase() === 'senior'){ 
+
+
+    const sect = document.getElementById("senior");
+    const divEl = document.createElement('div');
+    sect.appendChild(divEl);
+    const node = document.createTextNode (` ${this.image}  ,
+    Name: ${this.fullName} , ID : ${this.employeeId}  ,
+    Department : ${this.department} , Level : ${this.level}  , ${this.salary} `);
+    divEl.appendChild(node);
+
+    divEl.style.margin= '10px';
+    divEl.style.backgroundColor = '#F0F8FF';
+    divEl.style.height ='200px';
+    divEl.style.width ='200px';
+    divEl.style.border ='3px solid #333';
+   
+
+    } else if (this.level.toLowerCase() ==='mid-senior'){
+
+    const sect = document.getElementById("midSenior");
+    const divEl = document.createElement('div');
+    sect.appendChild(divEl);
+    const node = document.createTextNode(` ${this.image} , 
+    Name: ${this.fullName} , ID : ${this.employeeId}  ,
+    Department : ${this.department} , Level : ${this.level} ,  ${this.salary}  `);
+    divEl.appendChild(node);
+    
+
+    divEl.style.margin= '10px';
+    divEl.style.backgroundColor = '#F0F8FF';
+    divEl.style.height ='200px';
+    divEl.style.width ='200px';
+    divEl.style.border =' 3px solid #333';
+
+  
+
+    } else if (this.level.toLowerCase() ==='junior'){
+
+      const sect = document.getElementById("junior");
+      const divEl = document.createElement('div');
+      sect.appendChild(divEl);
+      const node = document.createTextNode(` ${this.image} ,   
+      Name: ${this.fullName} , ID : ${this.employeeId}  ,
+      Department : ${this.department} , Level : ${this.level}  , ${this.salary}  `);
+      divEl.appendChild(node);
+
+      divEl.style.display='flex';
+      divEl.style.margin= '10px';
+      divEl.style.backgroundColor = '#F0F8FF';
+      divEl.style.height ='200px';
+      divEl.style.width ='200px';
+      divEl.style.border = '3px solid #333';
+    
+    
+    }
+ 
+}
+
+for(let i = 0 ; i < allEmployee.length ; i++ ){
+  allEmployee[i].employeeCard();
+ 
 }
 
 
